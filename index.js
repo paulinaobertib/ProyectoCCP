c05c49979e2a68d39c55dee7b36e63c085df62bf
PauOberti <paulinaobertibusso@gmail.com>
2020-11-04 18:27:11 -0300

    se agregan estilos

---
+++ index.js
@@ -0,0 +1,10 @@
+alert("A COCINAR!!");
+
+var las_preparaciones=prompt ("Que te gustaria cocinar? budin, tarta, galletas, bizcochuelos?");
+
+if (las_preparaciones=="budin"||"tarta") {
+	alert("que delicia!")
+}
+else {
+	alert("que rico!")
+}
